e62b4bdd7b5927af110f149a2893af47c0865bd9
alvaro_cadiz <alvarocadiz10@gmail.com>
2024-05-21 15:49:20 +0200

    Registro y Inicio de sesion

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -1,5 +1,6 @@
 plugins {
     id("com.android.application")
+    id("com.google.gms.google-services")
 }
 
 android {
@@ -33,9 +34,18 @@ android {
 
 dependencies {
 
+    implementation(platform("com.google.firebase:firebase-bom:33.0.0"))
+    implementation("com.google.firebase:firebase-analytics")
+    implementation(platform("com.google.firebase:firebase-bom:32.8.0"))
+    implementation("com.google.firebase:firebase-auth")
+
+    implementation("com.google.firebase:firebase-database")
+
+
     implementation("androidx.appcompat:appcompat:1.6.1")
     implementation("com.google.android.material:material:1.10.0")
     implementation("androidx.constraintlayout:constraintlayout:2.1.4")
+    implementation("androidx.activity:activity:1.9.0")
     testImplementation("junit:junit:4.13.2")
     androidTestImplementation("androidx.test.ext:junit:1.1.5")
     androidTestImplementation("androidx.test.espresso:espresso-core:3.5.1")
---
+++ app/src/main/java/com/example/tfg/RegisterActivity.java
@@ -0,0 +1,103 @@
+package com.example.tfg;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import androidx.activity.EdgeToEdge;
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
+
+public class RegisterActivity extends AppCompatActivity {
+    FirebaseDatabase database = FirebaseDatabase.getInstance("https://tfg2024-dc712-default-rtdb.europe-west1.firebasedatabase.app/");
+    DatabaseReference myRef = database.getReference();
+
+    //FirebaseDatabase database = FirebaseDatabase.getInstance();
+    //DatabaseReference myRef = database.getReferenceFromUrl("https://tfg2024-dc712-default-rtdb.firebaseio.com/");
+   // DatabaseReference databaseReference = FirebaseDatabase.getInstance().getReferenceFromUrl("https://tfg2024-dc712-default-rtdb.europe-west1.firebasedatabase.app/");
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_register);
+
+        final EditText fullname = findViewById(R.id.fullname);
+        final EditText email = findViewById(R.id.email);
+        final EditText usuarios = findViewById(R.id.Usuario);
+        final EditText phone = findViewById(R.id.phone);
+        final EditText password = findViewById(R.id.password);
+        final EditText confirmPassword = findViewById(R.id.ConfirmPassword);
+
+        final Button registerBtn = findViewById(R.id.registerBtn);
+        final TextView loginNowBtn = findViewById(R.id.iniciaSesionYa);
+
+
+
+        registerBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                final String fullnameTxt = fullname.getText().toString();
+                final String emailTxt = email.getText().toString();
+                final String usuarioTxt = usuarios.getText().toString();
+                final String phoneTxt = phone.getText().toString();
+                final String passwordTxt = password.getText().toString();
+                final String confirmPasswordTxt = confirmPassword.getText().toString();
+
+                if(fullnameTxt.isEmpty() || emailTxt.isEmpty() || phoneTxt.isEmpty() || passwordTxt.isEmpty() || usuarioTxt.isEmpty()){
+                    Toast.makeText(RegisterActivity.this, "Porfavor rellena todas las celdas", Toast.LENGTH_SHORT).show();
+                }
+                else if(!passwordTxt.equals(confirmPasswordTxt)){
+                    Toast.makeText(RegisterActivity.this, "La contraaseña no coincide", Toast.LENGTH_SHORT).show();
+                }
+                else{
+                    myRef.child("usuarios").addListenerForSingleValueEvent(new ValueEventListener() {
+                        @Override
+                        public void onDataChange(@NonNull DataSnapshot snapshot) {
+
+
+                            if(snapshot.hasChild(usuarioTxt)){
+                                Toast.makeText(RegisterActivity.this, "Este usuario ya esta registrado", Toast.LENGTH_SHORT).show();
+                            }
+                            else{
+                                myRef.child("usuarios").child(usuarioTxt).child("fullname").setValue(fullnameTxt);
+                                myRef.child("usuarios").child(usuarioTxt).child("phone").setValue(phoneTxt);
+                                myRef.child("usuarios").child(usuarioTxt).child("password").setValue(passwordTxt);
+                                myRef.child("usuarios").child(usuarioTxt).child("email").setValue(emailTxt);
+                                Toast.makeText(RegisterActivity.this, "Usuario registrado correctamente", Toast.LENGTH_SHORT).show();
+                                finish();
+                            }
+                        }
+
+                        @Override
+                        public void onCancelled(@NonNull DatabaseError error) {
+
+                        }
+                    });
+
+
+                }
+            }
+        });
+        loginNowBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                finish();
+            }
+        });
+
+
+    }
+
+
+}

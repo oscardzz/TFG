625d767e6f44ad9f4eb54cff7be2fd94d348a58d
alvaro_cadiz <alvarocadiz10@gmail.com>
2024-06-17 20:37:47 +0200

    ACABADO DISEÑO Y HOMES

--- app/src/main/java/com/example/tfg/PlayerAdapter.java
+++ app/src/main/java/com/example/tfg/PlayerAdapter.java
@@ -60,20 +60,8 @@ public class PlayerAdapter extends ArrayAdapter<Player> {
                     .into(playerImage);
         }
 
-        // Configurar el clic en el elemento de la lista
-        convertView.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                if (mContext != null) {
-                    Player clickedPlayer = getItem(position);
-                    Intent intent = new Intent(mContext, PlayerDetailActivity.class);
-                    intent.putExtra("player", clickedPlayer);
-                    mContext.startActivity(intent);
-                } else {
-                    Log.e("PlayerAdapter", "Context is null");
-                }
-            }
-        });
+
+
 
         return convertView;
     }
